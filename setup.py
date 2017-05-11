709256a5bfaa720a1f7418af2c88794a2e4b5e5d
Fabio Zadrozny <fabiofz@gmail.com>
2017-05-11 08:16:58 -0300

    Fixing issue with pypi

--- setup.py
+++ setup.py
@@ -5,17 +5,16 @@ except ImportError:
 
 setup(
     name='py_compile_win_helpers',
-    version='1.0.0',
+    version='1.0.1',
     description = 'Helpers to setup the environment to compile extensions for Python',
     author='Fabio Zadrozny',
     url='https://github.com/fabioz/py_compile_win_helpers',
     py_modules=['py_compile_win_helpers'],
-    long_description=open('README.MD').read()
 )
 
 # Note: nice reference: https://jamie.curle.io/blog/my-first-experience-adding-package-pypi/
 # New version: change version and then:
-# git tag -a py_compile_win_helpers_1_0_0
+# git tag -a py_compile_win_helpers_1_0_1
 # git push --tags
 # python setup.py sdist
 # python setup.py sdist upload
